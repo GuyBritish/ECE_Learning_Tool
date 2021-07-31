2ffb170dbd869f7954b8403b62b917df33426353
Max <80323354+GuyBritish@users.noreply.github.com>
2021-07-31 14:47:17 +0700

    add input restrictions, reset button

--- public/scripts/allConverter.js
+++ public/scripts/allConverter.js
@@ -1,5 +1,7 @@
 const resultText = document.getElementById("result");
 const copyBtn = document.getElementById("copyToClipboard");
+const resetBtn = document.getElementById("resetBtn");
+const inputTxt = document.getElementsByClassName("inpText")[0];
 
 //=================================================================================================
 
@@ -10,5 +12,20 @@ copyBtn.addEventListener("click", function (event) {
 		window.getSelection().removeAllRanges();
 	} else if (document.selection) {
 		document.selection.empty();
+	}
+});
+
+resetBtn.addEventListener("click", function (event) {
+	resultText.innerHTML = "";
+	inputTxt.focus();
+});
+
+inputTxt.addEventListener("keydown", function (event) {
+	key = event.key.charCodeAt(0);
+	console.log(key);
+	if ((key >= 48 && key <= 57) || (key >= 65 && key <= 90) || (key >= 97 && key <= 122)) {
+		// do something
+	} else {
+		event.preventDefault();
 	}
 });
